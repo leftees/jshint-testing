0ebcb3ef8174433591d02a5a40c2e3a2dbec2ac3
leftees <jenna.olivia.smith@gmail.com>
2015-08-14 10:20:35 -0400

    add globals

--- test.js
+++ test.js
@@ -1,16 +1,15 @@
+
 function myDopeFunction(a, b) {
   var x = myAwesomeFunction(1, 2);
   var y = myReallyAwesomeFunction(5, 4);
   
-var nums = [];
+  var nums = [];
 
-for (var i = 0; i < 10; i++) {
-  (function (i) {
+  for (var i = 0; i < 10; i++) {
     nums[i] = function (j) {
-        return i + j;
+      return i + j;
     };
-  }(i));
-}
+  }
   
   nums[0](2); // Prints 12 instead of 2!!!
   
