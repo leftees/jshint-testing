f2088c9aac9f97f2c06d91b6e92fe007a8dcb0ce
leftees <jenna.olivia.smith@gmail.com>
2015-08-14 09:19:38 -0400

    fixed loop

--- test.js
+++ test.js
@@ -2,13 +2,15 @@ function myDopeFunction(a, b) {
   var x = myAwesomeFunction(1, 2);
   var y = myReallyAwesomeFunction(5, 4);
   
-  var nums = [];
+var nums = [];
 
-  for (var i = 0; i < 10; i++) {
+for (var i = 0; i < 10; i++) {
+  (function (i) {
     nums[i] = function (j) {
-      return i + j;
+        return i + j;
     };
-  }
+  }(i));
+}
   
   nums[0](2); // Prints 12 instead of 2!!!
   
